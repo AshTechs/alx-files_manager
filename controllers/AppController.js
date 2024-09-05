ed13969372695719da5e4f9469964e29eb34d319
AshTechs <66980494+AshTechs@users.noreply.github.com>
2024-09-05 02:56:58 +0000

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,24 +2,20 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static async getStatus(req, res) {
-    try {
-      const redisAlive = redisClient.isAlive();
-      const dbAlive = dbClient.isAlive();
-      res.status(200).json({ redis: redisAlive, db: dbAlive });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+  static getStatus(_req, res) {
+    const json = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).send(json);
   }
 
-  static async getStats(req, res) {
-    try {
-      const usersCount = await dbClient.nbUsers();
-      const filesCount = await dbClient.nbFiles();
-      res.status(200).json({ users: usersCount, files: filesCount });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+  static async getStats(_req, res) {
+    const json = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).send(json);
   }
 }
 
