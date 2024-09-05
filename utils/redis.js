33bc0ca49f2669e5bdefa78ee8c59ab4735eadab
AshTechs <ashbelh@gmail.com>
2024-09-05 01:01:41 +0000

    Done

--- utils/redis.js
+++ utils/redis.js
@@ -4,25 +4,48 @@ const { promisify } = require('util');
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.get = promisify(this.client.get).bind(this);
-    this.set = promisify(this.client.set).bind(this);
-    this.del = promisify(this.client.del).bind(this);
+    
+    this.getAsync = promisify(this.client.get).bind(this);
+    this.setAsync = promisify(this.client.set).bind(this);
+    this.delAsync = promisify(this.client.del).bind(this);
+
+    this.client.on('error', (err) => {
+      console.error('Redis client not connected to the server:', err.message);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
   }
 
   isAlive() {
     return this.client.connected;
   }
 
-  async getKey(key) {
-    return this.get(key);
+  async get(key) {
+    try {
+      const value = await this.getAsync(key);
+      return value;
+    } catch (err) {
+      console.error('Error getting key from Redis:', err.message);
+      return null;
+    }
   }
 
-  async setKey(key, value, duration = 86400) {
-    await this.set(key, value, 'EX', duration);
+  async set(key, value, duration = 86400) {
+    try {
+      await this.setAsync(key, value, 'EX', duration);
+    } catch (err) {
+      console.error('Error setting key in Redis:', err.message);
+    }
   }
 
-  async deleteKey(key) {
-    return this.del(key);
+  async del(key) {
+    try {
+      await this.delAsync(key);
+    } catch (err) {
+      console.error('Error deleting key from Redis:', err.message);
+    }
   }
 }
 
